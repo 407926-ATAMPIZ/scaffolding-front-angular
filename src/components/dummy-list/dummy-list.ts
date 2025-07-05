5a9eb92af4ee67dfa5fc3795db24b3050d4d27a5
407926-ATAMPIZ <407926@tecnicatura.frc.utn.edu.ar>
2025-07-04 21:15:46 -0300

    dummy-list con alguna correcciòn, comentarios en la clase del componente

--- src/components/dummy-list/dummy-list.ts
+++ src/components/dummy-list/dummy-list.ts
@@ -1,53 +1,59 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component, inject} from '@angular/core'; //inject para inyeccion sin constructor
 import {DummyService} from '../../services/dummy-service';
-import {BehaviorSubject, Observable, switchMap} from 'rxjs';
-import {AsyncPipe} from '@angular/common';
-import {FormBuilder, FormGroup, ReactiveFormsModule} from '@angular/forms';
-import {RouterLink} from '@angular/router';
+import {FormBuilder, FormGroup, ReactiveFormsModule} from '@angular/forms'; //para usar formularios reactivos
+import {Observable, startWith, Subject, switchMap} from 'rxjs'; //operadores rxjs para manejar flujos de datos reactivos.
+import {AsyncPipe} from '@angular/common'; //para trabajar con observables en el HTML
+import {Router} from '@angular/router'; //para redirecciones y navegaciòn
 
-@Component({
+@Component({ //define un componente standalone, su selector, imports, template y estilos
   selector: 'app-dummy-list',
-  imports: [
+  imports: [ //mòdulos necesarios para que el HTML pueda usar directivas y pipes como formGroup, formControlName, | async y routerLink
     AsyncPipe,
-    ReactiveFormsModule,
-    RouterLink
+    ReactiveFormsModule
   ],
   templateUrl: './dummy-list.html',
   styleUrl: './dummy-list.css'
 })
-export class DummyList implements OnInit {
-  private dummyService = inject(DummyService)
-  private formBuilder = inject(FormBuilder)
-  readonly filterForm = this.formBuilder.group({
+export class DummyList {
+  //inyeccion de dependencias alternativa al constructor
+  private dummyService = inject(DummyService); // DummyService para interactuar con la API
+  private formBuilder = inject(FormBuilder); // FormBuilder para crear el formulario
+  private router = inject(Router); //Router para redireccionar
+
+  // Formulario reactivo, define el FormGroup con dos campos opcionales
+  filterForm: FormGroup = this.formBuilder.group({
     dummyField: [''],
     fromDate: ['']
   })
-  private readonly filterTrigger$ = new BehaviorSubject<void>(undefined);
-
-  dummies$?: Observable<Dummy[]> = this.filterTrigger$.pipe(
-    switchMap(() => {
-      const {dummyField, fromDate} = this.filterForm.getRawValue();
+  // Subject para disparar la bùsqueda y carga de los dummies, un subject es un "disparador" manual. Emite valores con el Subject.next()
+  // Subject es un observable y observer al mismo tiempo, es un disparador manual muy útil.
+  private filterTrigger$ = new Subject<void>() // El tipo void indica que no se espera emitir datos, solo un "evento vacío"
+  //Lista de dummies como observable
+  dummies$: Observable<Dummy[]> = this.filterTrigger$.pipe( //cuando el subject hace .next() (emite un evento), se ejecuta el switchMap
+    startWith(null), //Para cargar autimaticamente al iniciar el componente
+    switchMap(() => { //elige si usar los filtros o traer todos los dummies, switchMap sirve para, en caso de que el observable emita una nueva señal, se cancela el accionar anterior y se sigue con el de la nueva señal.
+      const {dummyField, fromDate} = this.filterForm.value;
       const hasFilters = dummyField || fromDate;
       return hasFilters
-        ? this.dummyService.searchDummies(dummyField ?? undefined, fromDate ?? undefined)
-        : this.dummyService.getAll();
+        ? this.dummyService.searchDummies(dummyField || undefined, fromDate || undefined)
+        : this.dummyService.getAll()
     })
   )
 
-
-  ngOnInit() {
+  //activa el trigger al que està suscripto el observable de dummies
+  filter() {
     this.filterTrigger$.next();
   }
 
-  onFilter(): void {
-    this.filterTrigger$.next();
-  }
-
+  //llama al service, elimina un dummy por id y emite un evento del trigger para actualizar la tabla
   deleteDummy(id: number) {
-    this.dummyService.delete(id).subscribe({
-      error: err => console.log('deleteDummy failed:', err.message),
-    })
-    this.onFilter()
+    this.dummyService.delete(id).subscribe(() =>
+      this.filterTrigger$.next())
+  }
+
+  //redirige a la ruta /dummies/edit para actualizar un dummy (esa vista se encarga de buscar el dummy y cargar los campos del formulario)
+  updateDummy(id: number) {
+    this.router.navigate(['/dummies/edit', id]);
   }
 
 }
